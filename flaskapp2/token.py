f962df69093981f5d0ac7ce2cd812c47bc35d048
Shreeniwas Deshpande <shree.desh72@gmail.com>
2020-10-29 18:20:24 +0530

    Update token.py

--- flaskapp2/token.py
+++ flaskapp2/token.py
@@ -4,6 +4,7 @@ from flaskapp2 import app
 from flask_mail import Message
 from flaskapp2 import app
 import smtplib
+from twilio.rest import Client
 import os
 
 
@@ -37,3 +38,23 @@ def send_email(to, subject, template):
 
         msg = f'Subject: {subject}\n\n{template}'
         smtp.sendmail(email, to, msg)
+
+#Your new Phone Number is +12074642648
+def generateOTP(phno):
+    try:
+        account_sid = 'ACdde6653e686d036a4b77ac5f402ad523'
+        auth_token = '6e3b3b5f8bd09dc6c40b82340e809f19'
+        client = Client(account_sid, auth_token)
+        n = random.randint(1000, 9999)
+        message = client.messages \
+            .create(
+            body='OTP is - ' + str(n),
+            from_='+12074642648',
+            to= str(phno)
+        )
+
+        print(message.sid)
+        return n
+    except twilio.base.exceptions.TwilioRestException:
+        flash('Invalid Mobile number','danger')
+        return redirect(url_for('mobileform'))
